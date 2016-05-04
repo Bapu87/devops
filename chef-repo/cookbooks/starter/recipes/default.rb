41d9895c298a108f68f8c9bfb418b0bc708bd0e2
Bapu87 <rdelliker@gmail.com>
2016-05-04 17:05:18 -0400

    Added files via upload

---
+++ chef-repo/cookbooks/starter/recipes/default.rb
@@ -0,0 +1,27 @@
+# This is a Chef recipe file. It can be used to specify resources which will
+# apply configuration to a server.
+
+log "Welcome to Chef, #{node["starter_name"]}!" do
+  level :info
+end
+
+# For more information, see the documentation: https://docs.chef.io/essentials_cookbook_recipes.html
+
+package 'apache2'
+
+service 'apache2' do
+  supports :status => true
+  action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+                <head>
+        <title>Hello World</title>
+                </head>
+                <body>
+                <h1>Hello World!</h1>
+                </body>
+</html>'
+end
+
